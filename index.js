abb195a127ff141b2dfd82be42ce64d873b282a0
Salim <salimhetham@gmail.com>
2023-08-06 05:47:55 +0300

    progress

---
+++ index.js
@@ -0,0 +1,78 @@
+const carsApi = "http://localhost:3000/cars"
+
+let currentDisplayCarId = 1;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carList = document.getElementById('car-list');
+    const carNameElement = document.getElementById('car-name');
+    const carImageElement = document.getElementById('car-image');
+    const reviewList = document.getElementById('review-list');
+    carList.innerHTML = '';
+
+    fetch(carsApi)
+        .then((response) => response.json())
+        .then(function(cars) {
+            cars.forEach(function(car) {
+                const li = document.createElement('li')
+                li.textContent = car.name;
+
+                li.addEventListener('click', function() {
+                    fetch(`${carsApi}/${car.id}`)
+                        .then((response) => response.json())
+                        .then(function(carData) {
+                            carNameElement.textContent = carData.name;
+                            carImageElement.src = carData.image_url;
+                            reviewList.innerHTML = '';
+                            carData.reviews.forEach(function(review) {
+                                const li = document.createElement('li');
+                                li.textcontent = review;
+                                reviewList.appendChild(li);
+                            });
+
+                        })
+                        .catch((error) => (error));
+                    currentDisplayCarId = car.id;
+                });
+                carList.appendChild(li);
+            })
+        })
+        .catch((error) => (error));
+
+    fetch(`${carsApi}/${currentDisplayCarId}`)
+        .then((response) => response.json())
+        .then(function(carData) {
+            carNameElement.textContent = carData.name;
+            carImageElement.src = carData.image_url;
+
+            reviewForm.addEventListener('submit', function() {
+                event.preventDefault();
+                const reviewTextarea = document.getElementById('review');
+                const newReview = reviewTextarea.value;
+                const reviewList = document.getElementById('review-list');
+                const li = document.createElement('li');
+                li.textContent = newReview;
+                reviewList.appendChild(li);
+                reviewTextarea.value = '';
+                const beerId = currentDisplayCarId;
+                const reviewData = { review: newreview };
+
+                fetch(`${carsApi}/${beerId}/reviews`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify(reviewData)
+                    })
+                    .then((response) => response.json())
+                    .then((resposeData) => (resposeData))
+                    .catch((error) => (error));
+            })
+            reviewList.innerHTML = '';
+            beerData.reviews.forEach(function(review) {
+                const li = document.createElement('li');
+                li.textContent = review;
+                reviewList.appendChild(li);
+            })
+        })
+        .catch((error) => (error));
+})
